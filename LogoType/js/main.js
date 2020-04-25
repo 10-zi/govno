ed2b1dda3668b2866bc97a9be8beb431a5475998
Ilya Pavlov <62824522+10-zi@users.noreply.github.com>
2020-04-26 05:14:10 +1000

    Add files via upload

---
+++ LogoType/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	$('.slider_inner, .news_slider').slick({
+		nextArrow: '<button type="button" class="slick_btn slick_next"></button>',
+		prevArrow: '<button type="button" class="slick_btn slick_prev"></button>',
+		infinite: false,
+		
+
+
+});
+
+
+});
+
